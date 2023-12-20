31f107b9df72a2261f0a55de0b4c2129c3fcb54c
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-20 14:59:41 +0530

    module

---
+++ listA.py
@@ -0,0 +1,5 @@
+
+#listName = [elem1,elem2,elem3,...]
+#listName = [item1,item2,item3,...]
+
+fruits = ["apple", "banana", 'cherry', 'guava']
---
+++ listB.py
@@ -0,0 +1,5 @@
+# import moduelName
+import listA
+
+for fruit in listA.fruits:
+    print(fruit)
